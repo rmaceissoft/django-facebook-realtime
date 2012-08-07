d3dd53774251ca187feed74471ce9d643aecb81d
Reiner Marquez <rmaceissoft@gmail.com>
2012-08-06 19:27:34 -0500

    considering url conf functions for django<1.4

--- dj_facebook_realtime/urls.py
+++ dj_facebook_realtime/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls import patterns, url
+try:
+    from django.conf.urls import patterns, include, url
+except ImportError:
+    # https://docs.djangoproject.com/en/1.4/topics/http/urls/#module-django.conf.urls
+    from django.conf.urls.defaults import patterns, include, url
+
 
 urlpatterns = patterns('dj_facebook_realtime.views',
 
